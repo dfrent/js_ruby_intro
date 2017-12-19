c4b6dff9c99d57c9dbfad01bdfc6a39cb0e0862f
cordell <cordell.perry@gmail.com>
2017-12-19 14:45:12 -0500

    js syntax exercise

---
+++ convert.js
@@ -0,0 +1,75 @@
+
+// ruby code
+/* apples = 14
+puts apples
+
+puts "I have #{apples} apples."
+*/
+
+// js version
+// apples = 14
+// console.log(apples);
+// console.log(`I have ${apples} apples.`);
+
+// ruby code
+// materials = ['wood', 'metal', 'stone']
+// words = {
+//   'elephant' => "The world's largest land mammal.",
+//   'school' => 'A place of learning.',
+//   'ice cream' => 'A delicious milk-based dessert.',
+// }
+
+// js version
+
+// var materials = ['wood', 'metal', 'stone']
+//
+// var words = {
+//   elephant: "The world's largest land mammal.",
+//   school: 'A place of learning.',
+//   ice_cream: 'A delicious milk-based dessert.',
+// };
+//
+// console.log(words);
+
+// ruby code
+// num = 16
+// if num > 10
+//   puts "#{num} is greater than 10."
+// elsif num == 10
+//   puts "#{num} is exactly 10."
+// else
+//   puts "#{num} must be less than 10."
+// end
+
+// js code
+
+// num = 16
+//
+// if (num > 10) {
+//   console.log(`${num} is greater than 10.`)
+// } else if (num === 10) {
+//   console.log(`${num} is exactly 10`)
+// } else {
+//   console.log(`${num} must be less than 10.`)
+// };
+
+// ruby code
+// 10.times do
+//   puts "Doing the same thing over and over."
+// end\
+
+
+// js code
+
+for (var i = 0; i < 10; i++) {
+  console.log('doing things the same times over and over and over and over aND OVER');
+};
+
+// ruby code
+
+
+
+
+
+
+// js code 
